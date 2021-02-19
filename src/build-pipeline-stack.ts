de304a442e29b146857236713d7e921aef1c2576
Martin Mueller <damadden88@googlemail.com>
2021-02-19 12:56:22 +0100

    feat: add prod stage

--- src/build-pipeline-stack.ts
+++ src/build-pipeline-stack.ts
@@ -26,12 +26,8 @@ export class BuildPipelineStack extends core.Stack {
           },
         },
         artifacts: {
-          // 'base-directory': 'cdk.out',
           files: [
             'cdk.out/**/*',
-            // `${this.stackName}.template.json`,
-            // `${props.devStack.stackName}.template.json`,
-            // `${props.prodStack.stackName}.template.json`,
           ],
         },
       }),
@@ -43,30 +39,19 @@ export class BuildPipelineStack extends core.Stack {
     const sourceOutput = new codepipeline.Artifact();
     const cdkBuildOutput = new codepipeline.Artifact('CdkBuildOutput');
 
-    const deployProject = new codebuild.PipelineProject(this, 'updateStackDev', updateStack(props.devStack.stackName));
-    deployProject.addToRolePolicy(new iam.PolicyStatement({
-      actions: ['*'],
+    const deployDevProject = new codebuild.PipelineProject(this, 'updateStackDev', createUpdateStackSpec(props.devStack.stackName));
+    deployDevProject.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['*'], // cloudformation:DescribeStacks, ssm:GetParameter
       resources: ['*'],
     }));
 
-    const deploy = new codepipeline_actions.CodeBuildAction({
-      actionName: `${props.devStack.stackName}`,
-      project: deployProject,
-      input: cdkBuildOutput,
-    });
-
-    const pipelineRole = new iam.Role(this, 'DeployRole', {
-      assumedBy: new iam.ServicePrincipal('codepipeline.amazonaws.com'),
-      //managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')],
-    });
-
-    pipelineRole.addToPolicy(new iam.PolicyStatement({
-      actions: ['*'],
+    const deployProdProject = new codebuild.PipelineProject(this, 'updateStackProd', createUpdateStackSpec(props.prodStack.stackName));
+    deployProdProject.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['*'], // cloudformation:DescribeStacks, ssm:GetParameter
       resources: ['*'],
     }));
 
     new codepipeline.Pipeline(this, 'BuildPipeline', {
-      // role: pipelineRole,
       stages: [
         {
           stageName: 'Source',
@@ -110,25 +95,30 @@ export class BuildPipelineStack extends core.Stack {
         {
           stageName: 'DeployDev',
           actions: [
-            deploy,
+            new codepipeline_actions.CodeBuildAction({
+              actionName: `${props.devStack.stackName}`,
+              project: deployDevProject,
+              input: cdkBuildOutput,
+            }),
+          ],
+        },
+        {
+          stageName: 'DeployProd',
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: `${props.prodStack.stackName}`,
+              project: deployProdProject,
+              input: cdkBuildOutput,
+            }),
           ],
         },
       ],
       restartExecutionOnUpdate: true,
     });
-
-    // workaround https://stackoverflow.com/questions/63659802/cannot-assume-role-by-code-pipeline-on-code-pipeline-action-aws-cdk
-    // Altering cloudformation to remove role arn from actions
-    //   const pipelineCfn = pipeline.node.defaultChild as core.CfnResource;
-    //   // addDeletionOverride  removes the property from the cloudformation itself
-    //   // Delete action arn for every stage and action created
-    //   pipelineCfn.addDeletionOverride('Properties.Stages.1.Actions.0.RoleArn');
-    //   pipelineCfn.addDeletionOverride('Properties.Stages.2.Actions.0.RoleArn');
-    //   pipelineCfn.addDeletionOverride('Properties.Stages.3.Actions.0.RoleArn');
   }
 }
 
-function updateStack(stackName: string) {
+function createUpdateStackSpec(stackName: string) {
   return {
     buildSpec: codebuild.BuildSpec.fromObject({
       version: '0.2',
@@ -139,7 +129,7 @@ function updateStack(stackName: string) {
         },
         build: {
           commands: [
-            `cdk deploy --app 'cdk.out/' ${stackName} --require-approval never`, //--cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess`,
+            `cdk deploy --app 'cdk.out/' ${stackName} --require-approval never`,
           ],
         },
       },
